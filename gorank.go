fbb4b563d02bb5bf24eed1f72eafa08d68af57fc
Stephen Merity <smerity@smerity.com>
2015-03-16 15:13:58 -0700

    Remove channels as they're a major bottleneck (93s => 59s per iteration)

--- gorank.go
+++ gorank.go
@@ -15,46 +15,17 @@ type Edge struct {
 	To   uint32
 }
 
-func ReadU(r io.ByteReader) (uint64, error) {
-	var x uint64
-	var count int
-	//
-	b, err := r.ReadByte()
-	if err != nil {
-		return x, err
-	}
-	for b == 0 {
-		b, err = r.ReadByte()
-		if err != nil {
-			return x, err
-		}
-		count += 1
-	}
-	//
-	for c := 0; c < count; c++ {
-		x = (x << 8) + uint64(b)
-		b, err = r.ReadByte()
-		if err != nil {
-			return x, err
-		}
-	}
-	x = (x << 8) + uint64(b)
-	return x, nil
-}
-
-func sendEdges(filename string, hashOnSource bool, chans [](chan Edge), senderGroup *sync.WaitGroup) {
+func sendEdges(filename string, f func(uint32, uint32), senderGroup *sync.WaitGroup) {
 	defer senderGroup.Done()
-	f, _ := os.Open(filename)
-	defer f.Close()
-	gunzip, _ := gzip.NewReader(f)
+	file, _ := os.Open(filename)
+	defer file.Close()
+	gunzip, _ := gzip.NewReader(file)
 	// Adds the ReadByte method requird by io.ByteReader interface
 	wrappedByteReader := bufio.NewReader(gunzip)
 	edge := uint64(0)
-	chanLen := uint32(len(chans))
 	for {
 		// Read the variable integer and undo the delta encoding by adding the previous edge
 		rawEdge, err := binary.ReadUvarint(wrappedByteReader)
-		//rawEdge, err := ReadU(wrappedByteReader)
 		edge += rawEdge
 		if err == io.EOF {
 			break
@@ -68,41 +39,18 @@ func sendEdges(filename string, hashOnSource bool, chans [](chan Edge), senderGr
 		// Had (edge & 0xFFFFFFFF) for clarity but Go compiler doesn't optimize it away ...
 		eTo := uint32(edge)
 		// Edges are distributed across workers according to either source or destination node
-		if hashOnSource {
-			chans[eFrom%chanLen] <- Edge{eFrom, eTo}
-		} else {
-			chans[eTo%chanLen] <- Edge{eFrom, eTo}
-		}
+		f(eFrom, eTo)
 	}
 }
 
-func applyFunctionToEdges(f func(c chan Edge), workers int, hashOnSource bool) {
-	// The work for each of the workers is deposited onto their channel
-	chans := make([]chan Edge, workers, workers)
-	for i := range chans {
-		chans[i] = make(chan Edge, 1024)
-	}
-	//
+func applyFunctionToEdges(f func(uint32, uint32), workers int) {
 	var senderGroup sync.WaitGroup
 	for i := 0; i < 8; i++ {
 		senderGroup.Add(1)
-		go sendEdges(fmt.Sprintf("pld-arc.%d.bin.gz", i), hashOnSource, chans, &senderGroup)
-	}
-	//
-	var readerGroup sync.WaitGroup
-	for _, c := range chans {
-		readerGroup.Add(1)
-		go func(c chan Edge) {
-			defer readerGroup.Done()
-			f(c)
-		}(c)
+		go sendEdges(fmt.Sprintf("pld-arc.%d.bin.gz", i), f, &senderGroup)
 	}
 	//
 	senderGroup.Wait()
-	for _, c := range chans {
-		close(c)
-	}
-	readerGroup.Wait()
 }
 
 func main() {
@@ -116,11 +64,9 @@ func main() {
 	degree := make([]float32, total, total)
 	//
 	log.Printf("Calculating degree of each source node\n")
-	applyFunctionToEdges(func(c chan Edge) {
-		for edge := range c {
-			degree[edge.From] += 1
-		}
-	}, 8, true)
+	applyFunctionToEdges(func(from uint32, to uint32) {
+		degree[from] += 1
+	}, 8)
 	//
 	for iter := 0; iter < 20; iter++ {
 		log.Printf("PageRank Iteration: %d\n", iter+1)
@@ -130,10 +76,8 @@ func main() {
 			dest[i] = 1 - alpha
 		}
 		log.Printf("Calculating the probability mass gifted by incoming edges\n")
-		applyFunctionToEdges(func(c chan Edge) {
-			for edge := range c {
-				dest[edge.To] += src[edge.From]
-			}
-		}, 8, false)
+		applyFunctionToEdges(func(from uint32, to uint32) {
+			dest[to] += src[from]
+		}, 8)
 	}
 }
