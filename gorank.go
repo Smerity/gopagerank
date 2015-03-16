bd9f7174e152c1764dee02f39664ae8b41cb3fd4
Stephen Merity <smerity@smerity.com>
2015-03-16 01:22:25 -0700

    Fixed potential race condition and minor optimizations
    Race condition involved Edge(from, to) hashing. Hashing using "from" for
    calculating degree is good, hashing from "to" for calculating PageRank
    is good, but using one in the wrong spot results in potential race
    conditions.

--- gorank.go
+++ gorank.go
@@ -3,8 +3,8 @@ package main
 import "bufio"
 import "compress/gzip"
 import "encoding/binary"
-import "fmt"
 import "io"
+import "log"
 import "os"
 import "runtime"
 import "sync"
@@ -14,21 +14,18 @@ type Edge struct {
 	To   uint32
 }
 
-var workers = 31
-
-func sendEdges(filename string, chans [](chan Edge)) {
+func sendEdges(filename string, chans [](chan Edge), hashOnSource bool) {
 	f, _ := os.Open(filename)
 	defer f.Close()
 	gunzip, _ := gzip.NewReader(f)
 	// Adds the ReadByte method requird by io.ByteReader interface
 	wrappedByteReader := bufio.NewReader(gunzip)
-	i := 0
-	prevEdge := uint64(0)
+	edge := uint64(0)
+	chanLen := uint32(len(chans))
 	for {
 		// Read the variable integer and undo the delta encoding by adding the previous edge
 		rawEdge, err := binary.ReadUvarint(wrappedByteReader)
-		edge := rawEdge + prevEdge
-		prevEdge = edge
+		edge += rawEdge
 		if err == io.EOF {
 			break
 		}
@@ -37,13 +34,14 @@ func sendEdges(filename string, chans [](chan Edge)) {
 		}
 		// Seperate the two 32 bit nodes from the 64 bit edge
 		eFrom := uint32(edge >> 32)
-		eTo := uint32(edge & 0xFFFFFFFF)
-		// Edges are distributed across workers according to naive hash over source node
-		chans[eFrom%uint32(len(chans))] <- Edge{eFrom, eTo}
-		//
-		i++
-		if i%1e6 == 0 {
-			fmt.Println("Edge ", i)
+		// Converting uint64 to uint32 drops the top 32 bits
+		// Had (edge & 0xFFFFFFFF) for clarity but Go compiler doesn't optimize it away ...
+		eTo := uint32(edge)
+		// Edges are distributed across workers according to either source or destination node
+		if hashOnSource {
+			chans[eFrom%chanLen] <- Edge{eFrom, eTo}
+		} else {
+			chans[eTo%chanLen] <- Edge{eFrom, eTo}
 		}
 	}
 	for _, c := range chans {
@@ -51,16 +49,14 @@ func sendEdges(filename string, chans [](chan Edge)) {
 	}
 }
 
-func applyFunctionToEdges(f func(c chan Edge)) {
-	// The work for each of the workers is deposited onto their chansnnel
-	// A given node will only ever be mapped to a single worker
-	// theirhis allows preventing concurrency issues without locking
+func applyFunctionToEdges(f func(c chan Edge), workers int, hashOnSource bool) {
+	// The work for each of the workers is deposited onto their channel
 	chans := make([]chan Edge, workers, workers)
 	for i := range chans {
 		chans[i] = make(chan Edge, 1024)
 	}
 	//
-	go sendEdges("pld-arc.bin.gz", chans)
+	go sendEdges("pld-arc.bin.gz", chans, hashOnSource)
 	var wg sync.WaitGroup
 	for _, c := range chans {
 		wg.Add(1)
@@ -82,24 +78,25 @@ func main() {
 	dest := make([]float32, total, total)
 	degree := make([]float32, total, total)
 	//
+	log.Printf("Calculating degree of each source node\n")
 	applyFunctionToEdges(func(c chan Edge) {
 		for edge := range c {
 			degree[edge.From] += 1
 		}
-	})
+	}, 31, true)
 	//
 	for iter := 0; iter < 20; iter++ {
-		fmt.Println("PageRank Iteration:", iter)
-		fmt.Println("Calculating the source and destination vectors")
+		log.Printf("PageRank Iteration: %d\n", iter)
+		log.Printf("Calculating the source and destination vectors\n")
 		for i := range dest {
 			src[i] = alpha * dest[i] / degree[i]
 			dest[i] = 1 - alpha
 		}
-		fmt.Println("Calculating the probability mass gifted by incoming edges")
+		log.Printf("Calculating the probability mass gifted by incoming edges\n")
 		applyFunctionToEdges(func(c chan Edge) {
 			for edge := range c {
 				dest[edge.To] += src[edge.From]
 			}
-		})
+		}, 31, false)
 	}
 }
