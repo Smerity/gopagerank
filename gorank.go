4ea8e184acb91f5fb26c8f1f1d712bc8b09b1106
Stephen Merity <smerity@smerity.com>
2015-03-15 20:22:03 -0700

    Only calculates degree from gzipped edge list currently
    Mainly trying to improve performance as traversal is still too slow
    for a full 128 billion edge graph. There is a two times speedup by
    getting rid of the Edge channel but that means it's no longer generic...

---
+++ gorank.go
@@ -0,0 +1,82 @@
+package main
+
+import "bufio"
+import "compress/gzip"
+import "fmt"
+import "os"
+import "runtime"
+import "sync"
+
+type Edge struct {
+	From uint32
+	To   uint32
+}
+
+var workers = 32
+
+// Necessary as all other converters required allocating a string
+func bytesToUint32(s []byte) uint32 {
+	n := uint32(0)
+	p := uint32(1)
+	for i := range s {
+		n += uint32(s[len(s)-1-i]-'0') * p
+		p *= 10
+	}
+	return n
+}
+
+func sendEdges(filename string, chans [](chan Edge)) {
+	f, _ := os.Open(filename)
+	gunzip, _ := gzip.NewReader(f)
+	scanner := bufio.NewScanner(gunzip)
+	scanner.Split(bufio.ScanWords)
+	i := 0
+	for scanner.Scan() {
+		eFrom := bytesToUint32(scanner.Bytes())
+		scanner.Scan()
+		eTo := bytesToUint32(scanner.Bytes())
+		chans[eFrom%uint32(len(chans))] <- Edge{uint32(eFrom), uint32(eTo)}
+		//
+		i++
+		if i%1000000 == 0 {
+			fmt.Println("Edge ", i)
+		}
+	}
+	for _, c := range chans {
+		close(c)
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	var total uint32 = 42889799
+	//
+	/*
+		var src []uint32
+		var dest []uint32
+		src = make([]uint32, total, total)
+		dest = make([]uint32, total, total)
+	*/
+	var degree []uint32
+	degree = make([]uint32, total, total)
+	//
+	var chans = make([]chan Edge, workers, workers)
+	for i := range chans {
+		chans[i] = make(chan Edge, 256)
+	}
+	//
+	go sendEdges("pld-arc.gz", chans)
+	var wg sync.WaitGroup
+	for _, c := range chans {
+		wg.Add(1)
+		go func(c chan Edge) {
+			for edge := range c {
+				degree[edge.From] += 1
+			}
+			wg.Done()
+		}(c)
+	}
+	wg.Wait()
+	//
+	fmt.Println(degree[0])
+}
