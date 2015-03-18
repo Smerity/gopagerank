f059274a3e7bcbb7bf27a59f2e55e74644b45e56
Stephen Merity <smerity@smerity.com>
2015-03-18 02:46:56 -0700

    PageRank mass always sums to 1: accurate values + dangling node mass not lost
    + Major change: float32 => float64 for src + dest vectors - this
    increases the memory usage substantially but is required for precision
    + Missing probability mass from dangling nodes is retained &
    redistributed
    + Minor refactor: removing "workers" as there are no longer dynamic
    worker threads as the total number is the number of input files
    + Minor fix: first round of iteration was writing to src not dest

--- gorank.go
+++ gorank.go
@@ -54,7 +54,7 @@ func sendEdges(filename string, f func(uint32, uint32)) {
 	processEdgeStore(edgeStore, f)
 }
 
-func applyFunctionToEdges(f func(uint32, uint32), workers int) {
+func applyFunctionToEdges(f func(uint32, uint32)) {
 	var senderGroup sync.WaitGroup
 	totalParts := 4
 	for i := 0; i < totalParts; i++ {
@@ -73,10 +73,13 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	// We can either have total nodes supplied by the user or perform a full traversal of the data
 	total := uint32(42889799 + 1)
-	alpha := float32(0.85)
+	alpha := float64(0.85)
+	saveResults := false
 	//
-	src := make([]float32, total, total)
-	dest := make([]float32, total, total)
+	// floats must be 64 bit else we lose probability mass to floating point errors
+	// This is annoying as it substantially increases our in-memory requirements ...
+	src := make([]float64, total, total)
+	dest := make([]float64, total, total)
 	degree := make([]uint32, total, total)
 	//
 	log.Printf("Calculating degree of each source node\n")
@@ -84,35 +87,58 @@ func main() {
 		// Atomic is necessary here as each file is partitioned on the Edge(from, to)'s "to"
 		// Different workers may try to update the same "from" in a non-atomic fashion
 		atomic.AddUint32(&degree[from], 1)
-	}, 8)
+	})
 	//
 	// The first loop should have alpha set, else it's a noop as all src is equal to zero
+	// We also distribute the starting probability mass s.t. it totals one
 	for i := range dest {
-		src[i] = 1 - alpha
+		dest[i] = 1 / float64(total)
 	}
 	//
 	for iter := 0; iter < 20; iter++ {
 		log.Printf("PageRank Iteration: %d\n", iter+1)
 		log.Printf("Calculating the source and destination vectors\n")
+		// Calculate the probability mass that will be lost via dangling nodes
+		missingProb := float64(0)
+		for i := range degree {
+			if degree[i] == 0 {
+				missingProb += dest[i]
+			}
+		}
+		// Calculate the starting values
 		for i := range dest {
 			// If the node is dangling, src will equal +Inf due to degree being zero
 			// As the result is not used elsewhere, this isn't so much a problem
-			src[i] = alpha * (dest[i] / float32(degree[i]))
-			dest[i] = 1 - alpha
+			src[i] = alpha * (dest[i] / float64(degree[i]))
+			dest[i] = ((1 - alpha) / float64(total))
 		}
+		// Distribute the probability mass according to the edges
 		log.Printf("Calculating the probability mass gifted by incoming edges\n")
 		applyFunctionToEdges(func(from uint32, to uint32) {
 			dest[to] += src[from]
-		}, 8)
+		})
+		// Replace missing probability mass from dangling nodes
+		// (assumption is that they were equally distributed to all nodes)
+		for i := range dest {
+			dest[i] += alpha * (missingProb / float64(total))
+		}
 	}
 	// Write result
-	log.Printf("Saving results\n")
-	outf, _ := os.Create("result.txt")
-	defer outf.Close()
-	w := bufio.NewWriter(outf)
-	defer w.Flush()
-	for i, v := range dest {
-		w.WriteString(fmt.Sprintf("%d\t%f\n", i, v))
+	if saveResults {
+		log.Printf("Saving results\n")
+		outf, _ := os.Create("result.txt")
+		defer outf.Close()
+		w := bufio.NewWriter(outf)
+		defer w.Flush()
+		for i, v := range dest {
+			w.WriteString(fmt.Sprintf("%d\t%.12f\n", i, v))
+		}
+		log.Printf("Saved results\n")
 	}
-	log.Printf("Saved results\n")
+	//
+	totalProb := float64(0)
+	for _, v := range dest {
+		totalProb += v
+	}
+	log.Printf("Total probability mass: %f\n", totalProb)
 }
